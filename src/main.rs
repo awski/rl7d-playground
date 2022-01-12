eba23ec224246e061c7c94f8d110df21aa4f9dc6
Adam Wolanski <adam.wolanski@protonmail.com>
2022-01-12 23:35:23 +0100

    asd

--- src/main.rs
+++ src/main.rs
@@ -27,9 +27,11 @@ struct Object {
         Object { x, y, char, color }
     }
 
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
     pub fn draw(&self, console: &mut Offscreen) {
@@ -52,11 +54,23 @@ struct Tile {
     }
 }
 
-type Map = Vec<Vec<Tile>>;
-
 struct Game {
     map: Map
 }
+
+#[derive(Debug, Clone, Copy)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+} impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect { x1: x, y1: y, x2: x + w, y2: y + h }
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
 
 fn main() {
     let mut ctx = Tcod {
@@ -66,40 +80,36 @@ fn main() {
             .size(WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
             .title("rl7d playground")
             .init(),
-        con: Offscreen::new(WINDOW_WIDTH, WINDOW_HEIGHT)
+        con: Offscreen::new(MAP_WIDTH, MAP_HEIGHT)
     };
     tcod::system::set_fps(FPS_LIMIT as i32);
 
     let game = Game { map: make_map() };
-    let player = Object::new(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, '@', WHITE);
-    let npc = Object::new(WINDOW_WIDTH / 2 - 5, WINDOW_HEIGHT / 2, '@', YELLOW);
-    let mut objects = [player, npc];
+    let player = Object::new(25, 23, '@', WHITE);
+    let mut objects = [player];
+    // let npc = Object::new(WINDOW_WIDTH / 2 - 5, WINDOW_HEIGHT / 2, '@', YELLOW);
+    // let mut objects = [player, npc];
 
     while !ctx.root.window_closed() {
-        // ctx.con.set_default_foreground(tcod::colors::WHITE);
         ctx.con.clear();
-        
         render_all(&mut ctx, &game, &objects);
-        // for obj in &objects {
-        //     obj.draw(&mut ctx.con);
-        // }
 
         ctx.root.flush();
 
         let player = &mut objects[0];
-        let exit = handle_keys(&mut ctx, player);
+        let exit = handle_keys(&mut ctx, &game, player);
         if exit {
             return;
         }
     }
 }
 
-fn handle_keys(ctx: &mut Tcod, player: &mut Object) -> bool {
+fn handle_keys(ctx: &mut Tcod, game: &Game, player: &mut Object) -> bool {
     match ctx.root.wait_for_keypress(true) {
-        Key { code: KeyCode::Up, .. } => { player.move_by(0, -1) }
-        Key { code: KeyCode::Down, .. } => { player.move_by(0, 1) }
-        Key { code: KeyCode::Left, .. } => { player.move_by(-1, 0) }
-        Key { code: KeyCode::Right, .. } => { player.move_by(1, 0) }
+        Key { code: KeyCode::Up, .. } => { player.move_by(0, -1, game) }
+        Key { code: KeyCode::Down, .. } => { player.move_by(0, 1, game) }
+        Key { code: KeyCode::Left, .. } => { player.move_by(-1, 0, game) }
+        Key { code: KeyCode::Right, .. } => { player.move_by(1, 0, game) }
         Key { code: KeyCode::Enter, alt: true, .. } => { toggle_fullscreen(ctx) }
         Key { code: KeyCode::Escape, .. } => { return true }
         _ => {}
@@ -112,7 +122,7 @@ fn toggle_fullscreen(ctx: &mut Tcod) {
     ctx.root.set_fullscreen(!is_fullscreen)
 }
 
-fn make_map() -> Map {
+fn xx_make_map() -> Map {
     let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
     map[2][2] = Tile::wall();
@@ -121,8 +131,26 @@ fn make_map() -> Map {
     map
 }
 
-fn render_all(ctx: &mut Tcod, game: &Game, objects: &[Object]) {
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_map(room1, &mut map);
+    create_map(room2, &mut map);
+
+    map
+}
+
+fn create_map(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty()
+        }
+    }
+}
+
+fn render_all(ctx: &mut Tcod, game: &Game, objects: &[Object]) {
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
             let wall = game.map[x as usize][y as usize].block_sight;
